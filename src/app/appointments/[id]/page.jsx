19fec82c705ac2ef0d3a24b0983bce69750e8ac3
jakerul-islam <jakerulislam024@gmail.com>
2026-05-21 12:46:44 +0600

    error fixed

--- src/app/appointments/[id]/page.jsx
+++ src/app/appointments/[id]/page.jsx
@@ -1,10 +1,18 @@
 import DoctorDetails from "@/components/DoctorDetails";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 
 const DoctorsDetailsPage =async ({params}) => {
     const {id}= await params;
+   const {token }= await auth.api.getToken({headers: await headers()})
+   console.log(token ,'from details')
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/appoinments/${id}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/appoinments/${id}`,{
+        headers:{
+            "Authorization": `Bearer ${token}`
+        }
+    });
     const doctor = await res.json();
    
     return (
