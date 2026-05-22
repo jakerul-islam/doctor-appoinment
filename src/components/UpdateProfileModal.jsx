a233d671bcdc627d120e662f251d1788c261f412
jakerul-islam <jakerulislam024@gmail.com>
2026-05-22 15:31:28 +0600

    update profile bug fixed

--- src/components/UpdateProfileModal.jsx
+++ src/components/UpdateProfileModal.jsx
@@ -1,3 +1,6 @@
+
+
+
 "use client";
 
 import { authClient } from "@/lib/auth-client";
@@ -5,22 +8,32 @@ import { Button, Input, Label, Modal, Surface, TextField } from "@heroui/react";
 import { toast } from "react-toastify";
 
 export function UpdateProfileModal() {
-  const onSubmit = async(e)=>{
+  // 1. Current user-er session context theke data niye asha
+  const { data: session, isPending } = authClient.useSession();
+  const user = session?.user;
+
+  const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const userUpdate = Object.fromEntries(formData.entries());
-    console.log(userUpdate);
-
-    await authClient.updateUser({
-    image: userUpdate.image,
-    name: userUpdate.name
-
-})
-
-toast.success('updeted successfully')
-
     
-  }
+    // FormData input-er 'name' attribute thakle automatic value pay
+    const name = formData.get("name");
+    const image = formData.get("image");
+
+    try {
+      await authClient.updateUser({
+        image: image,
+        name: name,
+      });
+      toast.success('Updated successfully!');
+    } catch (error) {
+      toast.error('Something went wrong!');
+      console.error(error);
+    }
+  };
+
+  if (isPending) return <p>Loading...</p>;
+
   return (
     <Modal>
       <Button className={'w-full bg-teal-500 text-white rounded-none'} variant="secondary"> Update Profile </Button>
@@ -29,17 +42,20 @@ toast.success('updeted successfully')
           <Modal.Dialog className="sm:max-w-md">
             <Modal.CloseTrigger />
             <Modal.Header>
-              
               <Modal.Heading>Edit Profile</Modal.Heading>
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
                 <form onSubmit={onSubmit} className="flex flex-col gap-4">
-                  <TextField className="w-full" name="name" type="text">
+                  
+                  {/* 2. user?.name thakle sheta default value hishebe baddhiye deya */}
+                  <TextField defaultValue={user?.name || ""} className="w-full" name="name" type="text">
                     <Label>Name</Label>
                     <Input placeholder="Edit your name" />
                   </TextField>
-                  <TextField className="w-full" name="image" type="url">
+                  
+                  {/* 3. user?.image thakle sheta default value hishebe baddhiye deya */}
+                  <TextField defaultValue={user?.image || ""} className="w-full" name="image" type="url">
                     <Label>Image</Label>
                     <Input placeholder="Give Url" />
                   </TextField>
